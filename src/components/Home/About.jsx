5aa3bf4919ab578a0ebbb84e9584dac93589e8c0
tamilselvanrangasamy <tamilselvanrangasamy1@gmail.com>
2025-03-24 10:31:47 +0530

    Updates

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -12,58 +12,41 @@ const About = () => {
   return (
     <Container fluid className="home-about-section" id="about">
       <Container>
-        <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="yellow"> INTRODUCE </span> MYSELF
-            </h1>
-            <p className="home-about-body">
-              Hi, my name is <span className="yellow">Tamilselvan Rangasamy </span>
-              and I'm from <span className="yellow"> Chennai, Tamilnadu</span>
-              <br />
-              <br />
-              I recently graduated with a Master's degree in Engineering Design in 2024.
-              <br />
-              <br />
-              As a
-              <b className="yellow"> React </b>developer,
-              I enjoy tackling new challenges and continuously expanding my skillset.
-              <br />
-              <br />
-              A highly experienced and skilled <b className="yellow"> React developer</b> with a passion for building scalable, efficient, androbust applications. Proficient in<b className="yellow"> Material UI</b>, I craft visually stunning and responsive interfaces with ease. With expertise in <b className="yellow"> Jest and Enzyme </b>, I ensure top-notch code quality through rigorous and comprehensive unit testing. My skills include modular component-driven architecture, effective state management, and optimized performance
-             
-              <br />
-              <br />
-              I have a passion for working
-              with <b className="yellow">Node.js, MongoDB,</b> and
-              <i>
-                <b className="yellow">
-                  {" "}
-                  modern Javascript libraries and frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="yellow"> React.js</b>
-              </i>
-              <br />
-              <br />
-              I am also interested in building new
-              <i>
-                <b className="yellow"> Web Technologies and Products, </b>
-                as well as exploring areas related to
-                <b className="yellow"> Artificial Intelligence.</b>
-              </i>
-              <br />
+      <Row>
+  <Col md={8} className="home-about-description">
+    <h1 style={{ fontSize: "2.6em" }}>
+      LET ME <span className="yellow"> INTRODUCE </span> MYSELF
+    </h1>
+    <p className="home-about-body">
+      Hi, my name is <span className="yellow">Tamilselvan Rangasamy</span> and I'm from <span className="yellow">Chennai, Tamilnadu</span>
+      <br />
+      <br />
+      I recently graduated with a Master's degree in Engineering Design in 2024.
+      <br />
+      <br />
+      As a <b className="yellow">React</b> developer, I enjoy tackling new challenges and continuously expanding my skill set.
+      <br />
+      <br />
+      A highly experienced and skilled <b className="yellow">React developer</b> with a passion for building scalable, efficient, and robust applications. Proficient in <b className="yellow">TypeScript</b> and <b className="yellow">Next.js</b>, I create modern, scalable web applications with enhanced type safety and performance. I craft visually stunning and responsive interfaces using <b className="yellow">Material UI</b> and <b className="yellow">Tailwind CSS</b>, ensuring a seamless user experience.
+      <br />
+      <br />
+      With expertise in <b className="yellow">Jest</b> and <b className="yellow">Enzyme</b>, I ensure top-notch code quality through rigorous and comprehensive unit testing. My skills include modular component-driven architecture, effective state management, and optimized performance for modern web applications.
+      <br />
+      <br />
+      I am also passionate about working with <b className="yellow">Node.js</b>, <b className="yellow">MongoDB</b>, and modern JavaScript libraries and frameworks like <b className="yellow">React.js</b>.
+      <br />
+      <br />
+      I am also interested in building new <b className="yellow">Web Technologies and Products</b>, as well as exploring areas related to <b className="yellow">Artificial Intelligence</b>.
+      <br />
+    </p>
+  </Col>
+  <Col md={4} className="myAvtar">
+    <Tilt>
+      <img src={LaptopImg} className="img-fluid" alt="avatar" />
+    </Tilt>
+  </Col>
+</Row>
 
-            </p>
-          </Col>
-          <Col md={4} className="myAvtar">
-            <Tilt>
-              <img src={LaptopImg} className="img-fluid" alt="avatar" />
-            </Tilt>
-          </Col>
-        </Row>
         <Row>
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
--- src/pages/Skillset.jsx
+++ src/pages/Skillset.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { Container } from "react-bootstrap";
 
@@ -28,35 +27,4 @@ const Skillset = () => {
   )
 }
 
-=======
-import React from "react";
-import { Container } from "react-bootstrap";
-
-import Particle from '../components/Particle'
-import Techstack from "../components/Skillset/Techstack";
-import Toolstack from "../components/Skillset/Toolstack";
-import Github from "../components/Skillset/Github";
-
-const Skillset = () => {
-  return (
-    <Container fluid className="about-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          Professional <strong className="yellow">Skillset </strong>
-        </h1>
-
-        <Techstack />
-        <h1 className="project-heading">
-          <strong className="yellow">Tools</strong> I use
-        </h1>
-        <Toolstack />
-
-        <Github />
-      </Container>
-    </Container>
-  )
-}
-
->>>>>>> ee239e5ab2e4926d984648b0082eaf34d6254363
 export default Skillset
